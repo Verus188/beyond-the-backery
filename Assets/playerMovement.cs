0e6b4da3f100b61dbaa1b8f1528a0361bd31fd69
Nikita <89634774+Verus188@users.noreply.github.com>
2026-01-16 10:30:41 +0300

    исправление стрельбы
    пули летят в направлении курсора

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -6,14 +6,29 @@ public class PlayerMovement : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
 
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
     void Update()
     {
+        // Player movement
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         
         Vector2 movement = new Vector2(horizontal, vertical);
         transform.Translate(movement * speed * Time.deltaTime);
 
+        // Aiming
+        Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos - (Vector2)firePoint.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        firePoint.rotation = Quaternion.Euler(0, 0, angle);
+
+        // Shooting
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
