b6d0ac9b479dcce47f159b029cf368fd3deada63
Nikita <89634774+Verus188@users.noreply.github.com>
2026-01-22 22:42:43 +0300

    анимация бега для пряника в 4 направления

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -6,21 +6,47 @@ public class PlayerMovement : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
 
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     private Camera mainCamera;
 
     void Start()
     {
         mainCamera = Camera.main;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         // Player movement
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         
-        Vector2 movement = new Vector2(horizontal, vertical);
+        Vector2 movement = new Vector2(horizontal, vertical).normalized;
         transform.Translate(movement * speed * Time.deltaTime);
+
+        // Animation
+        if (movement.magnitude > 0)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.magnitude);
+
+            // Flip sprite for Left direction so we can reuse Right animation
+            if (movement.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (movement.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
 
         // Aiming
         Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
